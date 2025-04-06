4b3e911923d8bb33c44b524a9b18d33a6d8503ed
RahafJannuod <rahafjd14@gmail.com>
2025-04-06 19:09:57 +0200

    the second UI test for Fact2 buttom

--- FunFact/FunFactUITests/FunFactUITests.swift
+++ FunFact/FunFactUITests/FunFactUITests.swift
@@ -9,5 +9,11 @@ final class FunFactsAppUITests: XCTestCase {
         app.buttons["Fact 1"].tap()
         assert(app.staticTexts["The largest piece of fossilised dinosaur poo discovered is over 30cm long and over two litres in volume!"].exists, "the text is the not right one")
     }
+    func test_fact2() {
+        app.launch()
+        app.buttons["Fact 2"].tap()
+        assert(app.staticTexts["A chicken once lived for 18 months without a head!"].exists, "the text is the not right one")
+    }
+    
 }
 
