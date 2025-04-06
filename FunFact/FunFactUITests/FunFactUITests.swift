be71f4d9f7da959fd360403b352f981d698a1c7f
RahafJannuod <rahafjd14@gmail.com>
2025-04-06 19:43:26 +0200

    create a UI test for the Fact3 button and refactor the previous tests

--- FunFact/FunFactUITests/FunFactUITests.swift
+++ FunFact/FunFactUITests/FunFactUITests.swift
@@ -3,17 +3,25 @@ import XCTest
 final class FunFactsAppUITests: XCTestCase {
     let app = XCUIApplication()
     
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        app.launch()
+    }
     
     func test_fact1() {
-        app.launch()
         app.buttons["Fact 1"].tap()
-        assert(app.staticTexts["The largest piece of fossilised dinosaur poo discovered is over 30cm long and over two litres in volume!"].exists, "the text is the not right one")
-    }
-    func test_fact2() {
-        app.launch()
-        app.buttons["Fact 2"].tap()
-        assert(app.staticTexts["A chicken once lived for 18 months without a head!"].exists, "the text is the not right one")
+        XCTAssert(app.staticTexts["The largest piece of fossilised dinosaur poo discovered is over 30cm long and over two litres in volume!"].exists, "the text is the not right one")
     }
     
+    func test_fact2() {
+        app.buttons["Fact 2"].tap()
+        XCTAssert(app.staticTexts["A chicken once lived for 18 months without a head!"].exists, "the text is the not right one")
+    }
+    
+    func test_fact3() {
+        app.buttons["Fact 3"].tap()
+        XCTAssert(app.staticTexts["Animals can experience time differently from humans!"].exists, "the text is the not right one")
+    }
 }
 
