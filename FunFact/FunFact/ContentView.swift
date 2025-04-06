9eaf1eaf2acf99f085d11c559e435f2b62a41ef8
RahafJannuod <rahafjd14@gmail.com>
2025-04-06 16:01:50 +0200

    FunFact code

--- FunFact/FunFact/ContentView.swift
+++ FunFact/FunFact/ContentView.swift
@@ -1,24 +1,84 @@
-//
-//  ContentView.swift
-//  FunFact
-//
-//  Created by Rahaf Jannoud on 06.04.25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var displayText = "the fact is:"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            ZStack {
+                Color(.yellow)
+                    .ignoresSafeArea()
+                
+                VStack {
+                    Text("Fun Facts App")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(.red)
+                        .multilineTextAlignment(.leading)
+                    
+                    Text(displayText)
+                        .font(.largeTitle)
+                    
+                    Image("yelloe1")
+                        .resizable()
+                        .cornerRadius(11)
+                        .aspectRatio(contentMode: .fit)
+                    
+                    HStack(spacing: 20) {
+                        Button(action: {
+                            displayText = "The largest piece of fossilised dinosaur poo discovered is over 30cm long and over two litres in volume!"
+                        }) {
+                            Text("Fact 1")
+                                .fontWeight(.heavy)
+                                .padding()
+                                .background(Color.blue)
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                        }
+                        
+                        Button(action: {
+                            displayText = "A chicken once lived for 18 months without a head!"
+                        }) {
+                            Text("Fact 2")
+                                .fontWeight(.heavy)
+                                .padding()
+                                .background(Color.pink)
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                        }
+                        
+                        Button(action: {
+                            displayText = "Animals can experience time differently from humans!"
+                        }) {
+                            Text("Fact 3")
+                                .fontWeight(.heavy)
+                                .padding()
+                                .background(Color.green)
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                        }
+                    }
+                    
+                  
+                    NavigationLink(destination: AboutView()) {
+                        Text("About")
+                            .fontWeight(.heavy)
+                            .padding()
+                            .background(Color.black)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
+                    .padding(.top, 20)
+                }
+            }
+//            .navigationTitle("Home")
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
+
