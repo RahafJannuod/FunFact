93cdc10901a87a700735e44f23cd8eeb4226dbf7
RahafJannuod <rahafjd14@gmail.com>
2025-04-06 12:49:53 +0200

    setup funfact

---
+++ FunFact/FunFact/FunFactApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunFactApp.swift
+//  FunFact
+//
+//  Created by Rahaf Jannoud on 06.04.25.
+//
+
+import SwiftUI
+
+@main
+struct FunFactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FunFact/FunFactTests/FunFactTests.swift
@@ -0,0 +1,16 @@
+//
+//  FunFactTests.swift
+//  FunFactTests
+//
+//  Created by Rahaf Jannoud on 06.04.25.
+//
+
+import Testing
+
+struct FunFactTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
