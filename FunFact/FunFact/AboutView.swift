be9838690aa3b25f82d446cce2a92d5bba3aa30b
RahafJannuod <rahafjd14@gmail.com>
2025-04-07 21:48:16 +0200

    Fix safe area on smaller screens and update About view

--- FunFact/FunFact/AboutView.swift
+++ FunFact/FunFact/AboutView.swift
@@ -15,7 +15,7 @@ struct AboutView: View {
                                 Text("🟡 This app provides fun facts about different topics in a fun and interactive way.")
                                 Text("📱 Version: 1.0")
                                 Text("👩‍💻 Developed by: Rahaf Jannuod")
-                                Text("🗓️ Created in April 2025 as part of my SwiftUI learning journey.")
+                                Text("🗓️ Created in February 2025 as part of my SwiftUI learning journey.")
                                 Text("🛠️ Built using Xcode & SwiftUI.")
                                 Text("✅ You can find the test code and project files on GitHub:")
                                                 Link("👉 View it on GitHub", destination: URL(string: "https://github.com/RahafJannuod")!)
